bae8d8c688113e0f7d016519fb36c3e9d825a3b8
irumvanselme <andesanselme@gmail.com>
2021-08-08 18:06:37 +0200

    ADD: relationships to controllers

--- app/Models/AppointmentType.php
+++ app/Models/AppointmentType.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AppointmentType extends Model
 {
     use HasFactory;
 
     protected $casts = [];
+
+    public function appointments(): HasMany
+    {
+        return $this->hasMany(Appointment::class);
+    }
 }
